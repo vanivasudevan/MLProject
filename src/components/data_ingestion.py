3ccd159d0d8d783695f8d8c5f12678d0e65dded4
vanivasudevan <vasudevan.vani@gmail.com>
2024-04-27 17:58:22 +0530

    data transofmration

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass   # used to create class variable
 
+from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
 @dataclass  # can define the class varible directly without __init__
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifact','train.csv')
@@ -40,5 +42,7 @@ class DataIngestion:
 
 if __name__ == "__main__":
    obj = DataIngestion()
-   obj.initiate_data_ingestion()
+   train_data,test_data=obj.initiate_data_ingestion()
+   data_trainsformation = DataTransformation()
+   data_trainsformation.initiate_data_transformation(train_data,test_data)
         
